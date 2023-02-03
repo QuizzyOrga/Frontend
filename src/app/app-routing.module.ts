1844b8283653fbfe6aeac16f16a9dcd51e080116
ABOATDev <30906528+ABOATDev@users.noreply.github.com>
2023-02-03 14:04:43 +0100

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './profil/login/login.component';
+import { ProfilComponent } from './profil/profil.component';
+import { QuizzComponent } from './quizz/quizz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'quiz/:id',
+    component: QuizzComponent,
+  },
+  {
+    path: 'profil/:id',
+    component: ProfilComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  // {
+  //   path: 'search/:id',
+  //   component: RechercheComponent,
+  // },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
