e8352f8c7e51854fd2b8155216a9ba75f4116303
Eva1512 <82405496+Eva1512@users.noreply.github.com>
2023-02-17 14:48:49 +0100

    Ajout barre de recherche

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,9 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  searchTerm: string = "";
+  searchResults: any;
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    this.http.get<{results: any[]}>(`http://localhost:3000/recherche?term=${this.searchTerm}`)
+      .subscribe((response) => {
+        this.searchResults = response;
+        console.log(response)
+      });
+    }
   opened = false;
 
   log(state:any) {
     console.log(state)
   }
+
+  
 }
